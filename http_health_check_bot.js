f8c9d67bfa5bd71fa02ddf09d80467a869a096de
benjamin <benjamin@lunamint.com>
2019-03-27 17:20:14 +0900

    app2

--- http_health_check_bot.js
+++ http_health_check_bot.js
@@ -161,7 +161,7 @@ const siteJsonHelathCheck = (url)=>{
 //every 2 sec iterate
 const cron = new cronJob('*/2 * * * * *', function() {
 	appJsonHelathCheck(process.env.APP_URL)
-	app2JsonHelathCheck(process.env.APP_URL)
+	app2JsonHelathCheck(process.env.APP2_URL)
 	siteJsonHelathCheck(process.env.SITE_URL)
 }).start()
 
